acb691a31081ba97e4c39a4002f212ab404ee17a
AndriusBendoraitis <andrius.bendoraitis1985@gmail.com>
2019-11-28 22:31:22 +0200

    delete println

--- src/companyAndDevice/CompanyAndDeviceMain.java
+++ src/companyAndDevice/CompanyAndDeviceMain.java
@@ -13,18 +13,6 @@ public class CompanyAndDeviceMain {
     public static void main(String[] a) {
         List<Company> companyList = getCompanyListFromFile();
 
-        for (Company company : companyList) {
-            System.out.println(company.toString());
-        }
-
-        for (Company value : companyList) {
-            System.out.println(Math.max(value.getDevices().get(0).getPrice(), value.getDevices().get(1).getPrice()));
-        }
-        for (Company company: companyList){
-            if (company.getProductionType().equals("TECHNOLOGIES")){
-                System.out.println(company.toString());
-            }
-        }
     }
 
     private static List<Company> getCompanyListFromFile() {
